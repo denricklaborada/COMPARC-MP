3dd9e97868e13ad2fbfd3f296e74c497f1f9b4be
Denrick Laborada <denricklaborada@users.noreply.github.com>
2018-03-19 16:54:34 +0800

    added address

--- micromips/processor/views.py
+++ micromips/processor/views.py
@@ -1,62 +1,58 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
 import re
-# Create your views here.
 
 def index(request):
 	if request.method == 'POST':
-		commands = request.POST['commands']
-		cmdarr = commands.splitlines()
-		output = []
-		hexoutput = []
-		rangeLength = []
+		try:
+			commands = request.POST['commands']
+			cmdarr = commands.splitlines()
+			output = []
+			hexoutput = []
 
-		for command in cmdarr:
-			cmd = str(command.split(" ", 1)[0])
-			var1 = str(re.split(', ', command.split(" ", 1)[1])[0].split("R", 1)[1])
+			for command in cmdarr:
+				cmd = str(command.split(" ", 1)[0])
+				var1 = str(re.split(', ', command.split(" ", 1)[1])[0].split("R", 1)[1])
 
-			if cmd == "LD" or cmd == "SD":
-				var2 = str(re.split(', |\(|\)', command.split(" ", 1)[1])[1])
-			else:
-				var2 = str(re.split(', ', command.split(" ", 1)[1])[1].split("R", 1)[1])
+				if cmd == "LD" or cmd == "SD":
+					var2 = str(re.split(', |\(|\)', command.split(" ", 1)[1])[1])
+				else:
+					var2 = str(re.split(', ', command.split(" ", 1)[1])[1].split("R", 1)[1])
 
-			if cmd == "LD" or cmd == "SD":
-				var3 = str(re.split(', |\(|\)', command.split(" ", 1)[1])[2].split("R", 1)[1])
-			else:
-				var3 = str(re.split(r'R|#', re.split(', ', command.split(" ", 1)[1])[2])[1])
+				if cmd == "LD" or cmd == "SD":
+					var3 = str(re.split(', |\(|\)', command.split(" ", 1)[1])[2].split("R", 1)[1])
+				else:
+					var3 = str(re.split(r'R|#', re.split(', ', command.split(" ", 1)[1])[2])[1])
 
-			if cmd == "LD":
-				output.append('110111'+ format(int(var3), '05b') + format(int(var1), '05b') + format(int(var2[0], 16), '04b') + format(int(var2[1], 16), '04b') + format(int(var2[2], 16), '04b') + format(int(var2[3], 16), '04b'))
+				if cmd == "LD":
+					output.append('110111'+ format(int(var3), '05b') + format(int(var1), '05b') + format(int(var2[0], 16), '04b') + format(int(var2[1], 16), '04b') + format(int(var2[2], 16), '04b') + format(int(var2[3], 16), '04b'))
 
-			elif cmd == "SD":
-				output.append('111111' + format(int(var3), '05b') + format(int(var1), '05b') + format(int(var2[0]), '04b') + format(int(var2[1]), '04b') + format(int(var2[2]), '04b') + format(int(var2[3]), '04b'))
+				elif cmd == "SD":
+					output.append('111111' + format(int(var3), '05b') + format(int(var1), '05b') + format(int(var2[0]), '04b') + format(int(var2[1]), '04b') + format(int(var2[2]), '04b') + format(int(var2[3]), '04b'))
 
-			elif cmd == "DADDIU":
-				output.append('011001' + format(int(var2), '05b') + format(int(var1), '05b') + format(int(var3[0]), '04b') + format(int(var3[1]), '04b') + format(int(var3[2]), '04b') + format(int(var3[3]), '04b'))
+				elif cmd == "DADDIU":
+					output.append('011001' + format(int(var2), '05b') + format(int(var1), '05b') + format(int(var3[0]), '04b') + format(int(var3[1]), '04b') + format(int(var3[2]), '04b') + format(int(var3[3]), '04b'))
 
-			elif cmd == "XORI":
-				output.append('001110' + format(int(var2), '05b') + format(int(var1), '05b') + format(int(var3[0]), '04b') + format(int(var3[1]), '04b') + format(int(var3[2]), '04b') + format(int(var3[3]), '04b'))
+				elif cmd == "XORI":
+					output.append('001110' + format(int(var2), '05b') + format(int(var1), '05b') + format(int(var3[0]), '04b') + format(int(var3[1]), '04b') + format(int(var3[2]), '04b') + format(int(var3[3]), '04b'))
 
-			elif cmd == "DADDU":
-				output.append('000000' + format(int(var2), '05b') + format(int(var3), '05b') + format(int(var1), '05b') + '00000101101')
+				elif cmd == "DADDU":
+					output.append('000000' + format(int(var2), '05b') + format(int(var3), '05b') + format(int(var1), '05b') + '00000101101')
 
-			elif cmd == "SLT":
-				output.append('000000' + format(int(var2), '05b') + format(int(var3), '05b') + format(int(var1), '05b') + '00000101010')
+				elif cmd == "SLT":
+					output.append('000000' + format(int(var2), '05b') + format(int(var3), '05b') + format(int(var1), '05b') + '00000101010')
 
-			hexoutput.append(str(format(int(output[-1], 2), '08x')).upper())
+				hexoutput.append(str(format(int(output[-1], 2), '08x')).upper())
 
-		for i in range(len(cmdarr)):
-			rangeLength.append(i)
-
-		context = {
-			'commands': commands,
-			'cmdarr': cmdarr,
-			'output': output,
-			'hexoutput': hexoutput,
-			'rangeLength': rangeLength,
-		}
-		return render(request, 'processor/index.html', context)
+			context = {
+				'commands': commands,
+				'cmdarr': cmdarr,
+				'output': output,
+				'hexoutput': hexoutput,
+			}
+			return render(request, 'processor/index.html', context)
+			
+		except:
+			return render(request, 'processor/index.html')
 	
 	return render(request, 'processor/index.html')
